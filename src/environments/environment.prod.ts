daf87e67969c1f957f3ff90e82ae127876ab92a7
Joan Rojas <jarojasramirez147@gmail.com>
2021-06-24 12:23:05 -0500

    Ajuste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,4 @@ export const environment = {
   //Url Produccion
     urlA: "https://api.premiercredit.co:11444/PremierServices_API_EXT/api/login/authenticateEncoded",
     urlV: "https://api.premiercredit.co:11444/PremierServices_API_EXT/api/viabilizacion/getviabilizacion",
-
-  // urlA: "https://apitst.santanderconsumer.co:11445/PremierServices_API_EXT_TST/api/login/authenticateEncoded",
-  // urlV: "https://apitst.santanderconsumer.co:11445/PremierServices_API_EXT_TST/api/viabilizacion/getViabilizacionModular"
 };
