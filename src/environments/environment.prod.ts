db2ec8ba136cffa4b14022ef1daebf305d19795c
Joan Rojas <jarojasramirez147@gmail.com>
2021-06-24 11:09:10 -0500

    Ajuste env produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   urlApi: 'https://api.mercadolibre.com/items/',
   /* Urls */
   //Url Produccion
-  // urlA: "https://api.premiercredit.co:11444/PremierServices_API_EXT/api/login/authenticateEncoded",
-  // urlV: "https://api.premiercredit.co:11444/PremierServices_API_EXT/api/viabilizacion/getviabilizacion",
+    urlA: "https://api.premiercredit.co:11444/PremierServices_API_EXT/api/login/authenticateEncoded",
+    urlV: "https://api.premiercredit.co:11444/PremierServices_API_EXT/api/viabilizacion/getviabilizacion",
 
-  urlA: "https://apitst.santanderconsumer.co:11445/PremierServices_API_EXT_TST/api/login/authenticateEncoded",
-  urlV: "https://apitst.santanderconsumer.co:11445/PremierServices_API_EXT_TST/api/viabilizacion/getViabilizacionModular"
+  // urlA: "https://apitst.santanderconsumer.co:11445/PremierServices_API_EXT_TST/api/login/authenticateEncoded",
+  // urlV: "https://apitst.santanderconsumer.co:11445/PremierServices_API_EXT_TST/api/viabilizacion/getViabilizacionModular"
 };
